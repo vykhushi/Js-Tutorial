c93d7e9e6333f8efa802b1cb31490586bd3a5712
vykhushi <144993558+vykhushi@users.noreply.github.com>
2024-04-11 13:19:14 +0000

    converions

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+
+console.log(typeof(score));
+
+let valueInNumber= Number(score)//type is conervted into number
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber) //Nan 
+
+// "33"=33
+// "33abs"=NaN
+// true=false
+// null=0
+// undefined=nan
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber)
+
