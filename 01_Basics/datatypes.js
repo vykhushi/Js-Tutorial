27ed0d48484939490242d8a5fee178f6ec1d4f65
vykhushi <144993558+vykhushi@users.noreply.github.com>
2024-04-11 13:00:08 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version(new standards-classes modules)
+
+//alert(3+3) this will throw an error as we are using nodejs not browser
+
+let name ="Khushi" //string
+let age = 18 //number
+let isLoggedIn= true // Boolean 
+
+//  number= 2 to power 53
+//  bigint
+//  string => ""
+//  boolean= true/false 
+//  null => standalone value 
+//  undefined 
+//  symbol=>used for uniqeness
+
+//oject 
+
+console.log(typeof "khushi");//string 
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined 
+
